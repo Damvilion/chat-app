ff6bbb3742dc2c3d46a79a6cb149d63ceb30855e
Noel Ohaeri <noelohaeri@gmail.com>
2023-06-01 14:35:10 -0400

    Google sign in works + updated UI

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from "react";
 import Login from "./components/Login";
+import { useState } from "react";
+import { auth, provider } from "./firebase-config";
+import Chat from "./components/Chat";
+import Cookies from "universal-cookie/cjs/Cookies";
 
 function App() {
-  return (
-    <div className="App">
-      <Login />
-    </div>
-  );
+  const cookies = new Cookies();
+  const [Auth, setAuth] = useState(cookies.get("auth-token"));
+  if (!Auth) {
+    return (
+      <div className="App">
+        <Login Auth={Auth} setAuth={setAuth} />
+      </div>
+    );
+  }
+
+  return <Chat />;
 }
 
 export default App;
--- src/components/Login.js
+++ src/components/Login.js
@@ -2,18 +2,23 @@ import React from "react";
 import "./Login.css";
 import { auth, provider } from "../firebase-config";
 import { signInWithPopup } from "firebase/auth";
-const Login = () => {
+import Cookies from "universal-cookie";
+const Login = ({ Auth, setAuth }) => {
+  const cookies = new Cookies();
   const handleSubmit = async (e) => {
     try {
       const result = await signInWithPopup(auth, provider);
+      cookies.set("auth-token", result.user.email);
+      setAuth(true);
     } catch (err) {
       console.log(err);
     }
   };
   return (
     <div className="box">
+      <h1>Real Time Chat App</h1>
       <div className="login-container">
-        <h1>LOGIN</h1>
+        <h1 className="login-text">LOGIN</h1>
         <button onClick={handleSubmit} className="sign-in-button">
           Google Log In
         </button>
